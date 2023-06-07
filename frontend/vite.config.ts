824a82f46bd74b52fc1189e8df1630affbdd1287
JamelSyh <djamelmessi988@gmail.com>
2023-06-07 16:28:14 +0100

    bug fixe cors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8000'
-    }
-  },
   plugins: [react(), tsconfigPaths()],
 });
