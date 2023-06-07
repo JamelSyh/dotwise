17fbc8d6d3d244fbf2b94bfb9c9e8da03ff55072
JamelSyh <djamelmessi988@gmail.com>
2023-06-07 16:23:20 +0100

    bug fixe cors and proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000'
+    }
+  },
   plugins: [react(), tsconfigPaths()],
-  base: "https://dotwise.online/",
 });
