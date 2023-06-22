36f976ca9eb5215c99be3f858ce83ffcad64ec3d
JamelSyh <djamelmessi988@gmail.com>
2023-06-22 11:32:45 +0100

    deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dotenv from 'dotenv-vault-core'
+
+dotenv.config();
+console.log("hiiiii", process.env)
 
 // https://vitejs.dev/config/
 export default defineConfig({
