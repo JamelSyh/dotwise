f6ab7b94d3226a6de8334b20e20afa16b48b7084
JamelSyh <djamelmessi988@gmail.com>
2023-06-22 10:52:06 +0100

    prepare deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MyRouter from "routers/index";
 
 function App() {
+  console.log(import.meta.env.DOTWISE_API_KEY);
   return (
     <div className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
       <MyRouter />
