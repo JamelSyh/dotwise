6a912f88286349331deb60628af32de620986bd2
JamelSyh <djamelmessi988@gmail.com>
2023-06-22 11:43:04 +0100

    deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import MyRouter from "routers/index";
 
 function App() {
-  console.log("hiiiiiiiiiiiii", process.env.BASE_API_URL);
   return (
     <div className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
       <MyRouter />
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
