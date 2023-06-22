38b69f0989331a77b30e6646bf20df190353d3d5
JamelSyh <djamelmessi988@gmail.com>
2023-06-22 10:27:44 +0100

    prepare deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import MyRouter from "routers/index";
 
 function App() {
+  console.log("hiiiiiiiiiiiiiiiii");
+  console.log(import.meta.env.VITE_BASE_API_URL);
   return (
     <div className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
       <MyRouter />
